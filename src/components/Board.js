ef51d404cb5fa1c432b550ed460ca7bda8d94dd1
Dawidluna <dawidluna@gmail.com>
2022-01-08 13:29:03 +0100

    wyświetlanie figur

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,51 @@
 import React from 'react';
-
 import '../index.css';
 
-function Square(props) {
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            piece: "",
+            player: "",
+            image: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+        };
+    }
+
+    componentDidMount() {
+        if(this.props.row == 7 && (this.props.col == 0 || this.props.col == 7)) {
+            this.setState({piece: "rook", player: 1, image: "img/biala_wieza.png"});
+        } else if(this.props.row == 0 && (this.props.col == 0 || this.props.col == 7)) {
+            this.setState({piece: "rook", player: 2, image: "img/czarna_wieza.png"});
+        } else if(this.props.row == 7 && (this.props.col == 1 || this.props.col == 6)) {
+            this.setState({piece: "knight", player: 1, image: "img/bialy_skoczek.png"});
+        } else if(this.props.row == 0 && (this.props.col == 1 || this.props.col == 6)) {
+            this.setState({piece: "knight", player: 2, image: "img/czarny_skoczek.png"});
+        } else if(this.props.row == 7 && (this.props.col == 2 || this.props.col == 5)) {
+            this.setState({piece: "bishop", player: 1, image: "img/bialy_goniec.png"});
+        } else if(this.props.row == 0 && (this.props.col == 2 || this.props.col == 5)) {
+            this.setState({piece: "bishop", player: 2, image: "img/czarny_goniec.png"});
+        } else if(this.props.row == 7 && this.props.col == 3) {
+            this.setState({piece: "queen", player: 1, image: "img/biala_krolowa.png"});
+        } else if(this.props.row == 0 && this.props.col == 3) {
+            this.setState({piece: "queen", player: 2, image: "img/czarna_krolowa.png"});
+        } else if(this.props.row == 7 && this.props.col == 4) {
+            this.setState({piece: "king", player: 1, image: "img/bialy_krol.png"});
+        } else if(this.props.row == 0 && this.props.col == 4) {
+            this.setState({piece: "king", player: 2, image: "img/czarny_krol.png"});
+        } else if(this.props.row == 1) {
+            this.setState({piece: "pawn", player: 2, image: "img/czarny_pionek.png"});
+        } else if(this.props.row == 6) {
+            this.setState({piece: "pawn", player: 1, image: "img/bialy_pionek.png"});
+        }
+    }
+
+    render() {
     return(
-        <div id="field00" className={'field ' + props.color}>
-                <img src="#" alt="" />
+        <div id="field00" className={'field ' + this.props.color}>
+                <img src={this.state.image} alt="" />
             </div>
     )
+}
 }
 
 
