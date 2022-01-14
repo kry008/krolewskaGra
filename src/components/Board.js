e15a172628e03b0f57f9c4c47bd712ad30b4ca08
kry008 <pkrzysaaaa@gmail.com>
2022-01-14 19:17:45 +0100

    Expected '===' and instead saw '=='  eqeqeq
    WARNING in src\components\Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,29 +13,29 @@ class Square extends React.Component {
     }
 
     componentDidMount() {
-        if(this.props.row == 7 && (this.props.col == 0 || this.props.col == 7)) {
+        if(this.props.row === 7 && (this.props.col === 0 || this.props.col === 7)) {
             this.setState({piece: "rook", player: 1, image: "img/biala_wieza.png"});
-        } else if(this.props.row == 0 && (this.props.col == 0 || this.props.col == 7)) {
+        } else if(this.props.row === 0 && (this.props.col === 0 || this.props.col === 7)) {
             this.setState({piece: "rook", player: 2, image: "img/czarna_wieza.png"});
-        } else if(this.props.row == 7 && (this.props.col == 1 || this.props.col == 6)) {
+        } else if(this.props.row === 7 && (this.props.col === 1 || this.props.col === 6)) {
             this.setState({piece: "knight", player: 1, image: "img/bialy_skoczek.png"});
-        } else if(this.props.row == 0 && (this.props.col == 1 || this.props.col == 6)) {
+        } else if(this.props.row === 0 && (this.props.col === 1 || this.props.col === 6)) {
             this.setState({piece: "knight", player: 2, image: "img/czarny_skoczek.png"});
-        } else if(this.props.row == 7 && (this.props.col == 2 || this.props.col == 5)) {
+        } else if(this.props.row === 7 && (this.props.col === 2 || this.props.col === 5)) {
             this.setState({piece: "bishop", player: 1, image: "img/bialy_goniec.png"});
-        } else if(this.props.row == 0 && (this.props.col == 2 || this.props.col == 5)) {
+        } else if(this.props.row === 0 && (this.props.col === 2 || this.props.col === 5)) {
             this.setState({piece: "bishop", player: 2, image: "img/czarny_goniec.png"});
-        } else if(this.props.row == 7 && this.props.col == 3) {
+        } else if(this.props.row === 7 && this.props.col === 3) {
             this.setState({piece: "queen", player: 1, image: "img/biala_krolowa.png"});
-        } else if(this.props.row == 0 && this.props.col == 3) {
+        } else if(this.props.row === 0 && this.props.col === 3) {
             this.setState({piece: "queen", player: 2, image: "img/czarna_krolowa.png"});
-        } else if(this.props.row == 7 && this.props.col == 4) {
+        } else if(this.props.row === 7 && this.props.col === 4) {
             this.setState({piece: "king", player: 1, image: "img/bialy_krol.png"});
-        } else if(this.props.row == 0 && this.props.col == 4) {
+        } else if(this.props.row === 0 && this.props.col === 4) {
             this.setState({piece: "king", player: 2, image: "img/czarny_krol.png"});
-        } else if(this.props.row == 1) {
+        } else if(this.props.row === 1) {
             this.setState({piece: "pawn", player: 2, image: "img/czarny_pionek.png"});
-        } else if(this.props.row == 6) {
+        } else if(this.props.row === 6) {
             this.setState({piece: "pawn", player: 1, image: "img/bialy_pionek.png"});
         }
     }
@@ -77,8 +77,8 @@ export default class Board extends React.Component {
     }
 
     squareClicked(row, col, piece, player) {
-        if(this.state.firstClickCol == "") {
-            if(player == this.state.whichPlayer) {
+        if(this.state.firstClickCol === "") {
+            if(player === this.state.whichPlayer) {
             this.setState({firstClickCol: col, firstClickRow: row, firstClickPiece: piece, firstClickPlayer: player});
             } else console.log("nie ten gracz");
         } else this.doMove();
@@ -90,7 +90,7 @@ export default class Board extends React.Component {
         for (let i = 0; i < 8; i++) {
             const row = [];
             for (let j = 0; j < 8; j++) {
-                if((i%2 + j%2)%2 == 0)
+                if((i%2 + j%2)%2 === 0)
                 {
                     row.push(<Square color="white" col={j} row={i} clicked={this.squareClicked} />)
                 }
