52970ed1a0ff446090a532c070c93c7bf5e06ea9
kry008 <pkrzysaaaa@gmail.com>
2022-01-25 10:21:42 +0100

    Dodanie Square.js, React.StrictMode

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,7 @@
 import React from 'react';
 import '../index.css';
+import Square from './Square'
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            image: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-        };
-        this.clicked = this.clicked.bind(this);
-    }
-    clicked() {
-        this.props.clicked(this.props.row, this.props.col);
-    }
-    componentDidMount() {
-        if(this.props.player == "1" && this.props.piece == "pawn") this.setState({image: "img/bialy_pionek.png"});
-        else if(this.props.player == "2" && this.props.piece == "pawn") this.setState({image: "img/czarny_pionek.png"});
-        else if(this.props.player == "1" && this.props.piece == "rook") this.setState({image: "img/biala_wieza.png"});
-        else if(this.props.player == "2" && this.props.piece == "rook") this.setState({image: "img/czarna_wieza.png"});
-        else if(this.props.player == "1" && this.props.piece == "knight") this.setState({image: "img/bialy_skoczek.png"});
-        else if(this.props.player == "2" && this.props.piece == "knight") this.setState({image: "img/czarny_skoczek.png"});
-        else if(this.props.player == "1" && this.props.piece == "bishop") this.setState({image: "img/bialy_goniec.png"});
-        else if(this.props.player == "2" && this.props.piece == "bishop") this.setState({image: "img/czarny_goniec.png"});
-        else if(this.props.player == "1" && this.props.piece == "queen") this.setState({image: "img/biala_krolowa.png"});
-        else if(this.props.player == "2" && this.props.piece == "queen") this.setState({image: "img/czarna_krolowa.png"});
-        else if(this.props.player == "1" && this.props.piece == "king") this.setState({image: "img/bialy_krol.png"});
-        else if(this.props.player == "2" && this.props.piece == "king") this.setState({image: "img/czarny_krol.png"});
-        else this.setState({image: "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="});
-    }
-    render() {
-        return(
-            <div id="field00" onClick={this.clicked} className={'field ' + this.props.color} style={{ 
-                backgroundImage: `url("${this.state.image}")` 
-            }}>
-                </div>
-        )
-    }
-}
 export default class Board extends React.Component {
     constructor(props) {
         super(props);
--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
+  <React.StrictMode>
   <div id="page">
     Szachy React
     <Game />
-</div>,
+  </div>
+</React.StrictMode>,
   document.getElementById('root')
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
